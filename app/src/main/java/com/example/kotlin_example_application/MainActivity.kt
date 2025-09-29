478e0d3aafe3bf307d6aba0c99ae00097b8eb919
Mikhail Toropchinov <ruhose73@gmail.com>
2025-09-29 21:29:44 +0300

    feat(application): add basic project structure with screens

--- app/src/main/java/com/example/kotlin_example_application/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_example_application/MainActivity.kt
@@ -3,45 +3,15 @@ package com.example.kotlin_example_application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlin_example_application.ui.theme.Kotlin_example_applicationTheme
+import androidx.navigation.compose.rememberNavController
+import com.example.kotlin_example_application.ui.navigation.AppNavGraph
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Kotlin_example_applicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            val navController = rememberNavController()
+            AppNavGraph(navController)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Kotlin_example_applicationTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/kotlin_example_application/ui/components/BottomNavBar.kt
@@ -0,0 +1,41 @@
+package com.example.kotlin_example_application.ui.components
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.kotlin_example_application.ui.navigation.bottomNavItems
+
+@Composable
+fun BottomNavBar(navController: NavController) {
+    val navBackStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry.value?.destination?.route
+
+    NavigationBar(containerColor = Color.White) {
+        bottomNavItems.forEach { screen ->
+            NavigationBarItem(
+                selected = currentRoute == screen.route,
+                onClick = {
+                    if(currentRoute != screen.route) {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                inclusive = false
+                            }
+                            launchSingleTop = true
+                        }
+                    }
+                },
+                icon = {
+                    screen.icon?.let {
+                        Icon(it, contentDescription = screen.title)
+                    }
+                },
+                label = {screen.title?.let {Text(it)}}
+            )
+        }
+    }
+}
