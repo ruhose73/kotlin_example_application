ee6e8ec95bdddcbf21f2893a422aa351ffb32ea8
ruhose73 <ruhose73@gmail.com>
2025-09-29 21:41:05 +0300

    feat(profile): add profile repository to screen

--- app/src/main/java/com/example/kotlin_example_application/ui/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/kotlin_example_application/ui/screens/ProfileScreen.kt
@@ -7,12 +7,29 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.kotlin_example_application.data.models.ProfileDataSpec
+import com.example.kotlin_example_application.data.repository.ProfileRepository
 
 @Composable
 fun ProfileScreen(navControlller: NavController) {
+
+    var refreshing by remember { mutableStateOf(true) }
+    var profile by remember { mutableStateOf<ProfileDataSpec?>(null) }
+    val repository = remember { ProfileRepository() }
+
+    LaunchedEffect(Unit) {
+        refreshing = true
+        profile = repository.getProfileData()
+        refreshing = false
+    }
 
     Column(
         modifier = Modifier
@@ -20,9 +37,10 @@ fun ProfileScreen(navControlller: NavController) {
             .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
-
-        Text("Профиль", style = MaterialTheme.typography.headlineMedium)
-        Text("Пользователь: Тестовый пользователь")
-        Text("Почта: test@yandex.com")
+        profile?.let { data ->
+            Text("Профиль", style = MaterialTheme.typography.headlineMedium)
+            Text("Пользователь: ${data.name}")
+            Text("Почта: ${data.email}")
+        }
     }
 }
